c9cc84e7d505e96c76391a934a5a6c241e91b6a6
Hardeep Nain <88960105+HardeepNain@users.noreply.github.com>
2023-05-07 19:07:40 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (32.97%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        //Your code here
+        
+        int a = 0, b = 1, c = 0, sum = 0;
+        for(int i =0;i<n;i++)
+        {
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+        return a;
+        
+    }
+}
