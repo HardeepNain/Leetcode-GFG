1cc6ea07a831e18767e1f4c17c9806acce5b5495
Hardeep Nain <88960105+HardeepNain@users.noreply.github.com>
2023-05-23 17:21:44 +0530

    Added solution - LeetHub

---
+++ Insertion Sort - GFG/insertion-sort.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.Math;
+
+class Sorting
+{
+	
+	static void printArray(int arr[],int size)
+	{
+		int i;
+		for(i=0;i<size;i++)
+		System.out.print(arr[i]+" ");
+	    System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+		
+			for(int i=0;i<n;i++)
+			a[i]= sc.nextInt();
+			
+			 new Solution().insertionSort(a,n);
+			 printArray(a,n);
+			
+		t--;
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+  static void insert(int arr[],int i)
+  {
+       // Your code here
+  }
+  //Function to sort the array using insertion sort algorithm.
+  public void insertionSort(int arr[], int n)
+  {
+      //code here
+        for(int itr = 1; itr <= n-1 ; itr++){  // same logic of BS
+            for(int j = itr ; j > 0 ; j--){
+                if(isGreater(arr,j-1,j)){
+                    swap(arr,j-1,j);
+                }else{
+                    break;
+                }
+            }
+        }
+    
+    }
+    
+    // used for swapping ith and jth elements of array
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // return true if jth element is greater than ith element
+    public static boolean isGreater(int[] arr, int j, int i) {
+        if (arr[i] < arr[j]) {
+        return true;
+        } else {
+        return false;
+        }
+    }
+}
