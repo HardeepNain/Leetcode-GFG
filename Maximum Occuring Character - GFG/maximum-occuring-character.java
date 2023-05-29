bbb86aa2b33ad09e10a6fb0daca091b5f18bc159
Hardeep Nain <88960105+HardeepNain@users.noreply.github.com>
2023-05-29 16:43:55 +0530

    Added solution - LeetHub

--- Maximum Occuring Character - GFG/maximum-occuring-character.java
+++ Maximum Occuring Character - GFG/maximum-occuring-character.java
@@ -30,34 +30,64 @@ class Solution
     public static char getMaxOccuringChar(String line)
     {
         // Your code here
-        //BRUTE FORCE
+        
+        // //BRUTE FORCE
+        
+        // char hiFreqChar = ' ';
+        // int maxFreq = 0;
+        
+        // for(int i= 0; i<line.length(); i++){
+            
+        //     char ch = line.charAt(i);
+        //     int freq = 1;
+            
+        //     for(int j = i; j<line.length(); j++){
+        //         if(line.charAt(j) == ch){
+        //             freq++;
+        //         }
+        //         count++;
+        //     }
+            
+        //     // System.out.println(ch +" "+freq);
+            
+        //     if(freq>maxFreq){
+        //         maxFreq = freq;
+        //         hiFreqChar = ch;
+        //     }
+        //     else if(freq == maxFreq && ch < hiFreqChar){
+        //         hiFreqChar = ch;
+        //     }
+        // }
+        // return hiFreqChar;
+        
+        
+        // Optimized
+        HashMap<Character,Integer> hm = new HashMap<>();
         
         char hiFreqChar = ' ';
         int maxFreq = 0;
         
         for(int i= 0; i<line.length(); i++){
             
-            char ch = line.charAt(i);
-            int freq = 1;
+            char ch =line.charAt(i);
             
-            for(int j = i; j<line.length(); j++){
-                if(line.charAt(j) == ch){
-                    freq++;
-                }
+            if(hm.containsKey(ch)){
+                hm.put(ch,hm.get(ch)+1);
+            }
+            else{
+                hm.put(ch,1);
             }
             
-            // System.out.println(ch +" "+freq);
-            
-            if(freq>maxFreq){
-                maxFreq = freq;
+            if(hm.get(ch)>maxFreq){
+                maxFreq = hm.get(ch);
                 hiFreqChar = ch;
             }
-            else if(freq == maxFreq && ch < hiFreqChar){
+            else if(hm.get(ch) == maxFreq && ch < hiFreqChar){
                 hiFreqChar = ch;
             }
         }
         
         return hiFreqChar;
-    }
     
+    }
 }
