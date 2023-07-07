89a2bf17307a4c2a4aea05121b372229c11438f4
Hardeep Nain <88960105+HardeepNain@users.noreply.github.com>
2023-07-07 12:18:25 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().count(arr, n, x);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+
+    public static int firstOccurrence(int[] arr, int n, int k) {
+        int low = 0, high = n - 1;
+        int first = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] == k) {
+                first = mid;
+                // look for smaller index on the left
+                high = mid - 1;
+            } else if (arr[mid] < k) {
+                low = mid + 1; // look on the right
+            } else {
+                high = mid - 1; // look on the left
+            }
+        }
+        return first;
+    }
+
+    public static int lastOccurrence(int[] arr, int n, int k) {
+        int low = 0, high = n - 1;
+        int last = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] == k) {
+                last = mid;
+                // look for larger index on the right
+                low = mid + 1;
+            } else if (arr[mid] < k) {
+                low = mid + 1; // look on the right
+            } else {
+                high = mid - 1; // look on the left
+            }
+        }
+        return last;
+    }
+
+    public static int[] firstAndLastPosition(int[] arr, int n, int k) {
+        int first = firstOccurrence(arr, n, k);
+        if (first == -1) return new int[] { -1, -1};
+        int last = lastOccurrence(arr, n, k);
+        return new int[] {first, last};
+    }
+
+    int count(int arr[], int n, int x) {
+        int[] ans = firstAndLastPosition(arr, n, x);
+        if (ans[0] == -1) return 0;
+        return (ans[1] - ans[0] + 1);
+    }
+}
